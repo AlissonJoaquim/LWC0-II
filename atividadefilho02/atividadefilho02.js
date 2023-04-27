e5052aaa08354f0c7a3a7edcdbc22d7dd9f0cd31
AlissonJoaquim <128437734+AlissonJoaquim@users.noreply.github.com>
2023-04-27 08:54:21 -0300

    Add files via upload

---
+++ atividadefilho02/atividadefilho02.js
@@ -0,0 +1,8 @@
+import { LightningElement, api } from 'lwc';
+
+export default class Atividadefilho02 extends LightningElement {
+    @api ide;
+    @api name;
+    @api amount;
+    @api stagename;
+}
