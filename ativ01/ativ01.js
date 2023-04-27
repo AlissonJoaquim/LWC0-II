2274b2642700660c5322daf41d37bd1b8a3c704b
AlissonJoaquim <128437734+AlissonJoaquim@users.noreply.github.com>
2023-04-27 08:35:03 -0300

    Add files via upload

---
+++ ativ01/ativ01.js
@@ -0,0 +1,14 @@
+import { LightningElement, api } from 'lwc';
+
+export default class ativ01 extends LightningElement {
+    pe = [
+        {id: 51226,
+        primeironome: "NANI", 
+        sobrenome: "massahiko", 
+        idade: "22", 
+        email: "renanzinho@bigoditu.cuom", 
+        classificacao: "hot", 
+        empresa: "Salesforce", 
+        receitaanual: 2000000},
+        ];
+}
