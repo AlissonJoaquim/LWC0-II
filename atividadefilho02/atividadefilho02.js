0a7fa323f1d923e661ec3e62ff4ad55e630b5254
AlissonJoaquim <128437734+AlissonJoaquim@users.noreply.github.com>
2023-04-27 09:39:42 -0300

    Delete atividadefilho02 directory

--- atividadefilho02/atividadefilho02.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class Atividadefilho02 extends LightningElement {
-    @api ide;
-    @api name;
-    @api amount;
-    @api stagename;
-}
